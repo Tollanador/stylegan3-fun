6a5c412f24fc3505610fbc8de1bb4fb92d53f689
Tollanador <45375315+Tollanador@users.noreply.github.com>
2021-12-11 09:05:48 +1000

    Fixed all TPU stuff
    Tested with GPU and no TPU - working.

--- train.py
+++ train.py
@@ -70,10 +70,10 @@ def subprocess_fn(rank, c, temp_dir, all_opts):
     #if c.num_gpus > 1:
     if use_tpu==None and c.num_gpus > 1:
         init_file = os.path.abspath(os.path.join(temp_dir, '.torch_distributed_init'))
-    if os.name == 'nt':
+    	if os.name == 'nt':
             init_method = 'file:///' + init_file.replace('\\', '/')
             torch.distributed.init_process_group(backend='gloo', init_method=init_method, rank=rank, world_size=c.num_gpus)
-    else:
+   	 else:
             init_method = f'file://{init_file}'
             #torch.distributed.init_process_group(backend='nccl', init_method=init_method, rank=rank, world_size=c.num_gpus)
             torch.distributed.init_process_group(backend='nccl' if use_tpu == None else 'xla-tpu', init_method=init_method, rank=rank, world_size=c.num_gpus)
